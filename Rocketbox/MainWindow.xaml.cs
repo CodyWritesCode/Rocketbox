b3ee1e3cf0f7cc1b2c8670946118d1c7a13d095a
DocY <cody@ydoc.ca>
2017-07-24 20:39:00 -0500

    initial commit with basic window

---
+++ Rocketbox/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rocketbox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Deactivated += (sender, e) => this.Close();
+            this.KeyDown += KeyPress;
+        }
+
+        private void KeyPress(object sender, KeyEventArgs e)
+        {
+            switch(e.Key)
+            {
+                case Key.Escape:
+                    this.Close();
+                    break;
+            }
+        }
+    }
+}
