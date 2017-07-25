44503d1c4c9d1201a6be404ac9e0558dd0da6bb5
Cody <codywritescode@gmail.com>
2017-07-25 00:04:00 -0500

    implement shell execute command

--- Rocketbox/Commands.cs
+++ Rocketbox/Commands.cs
@@ -55,4 +55,43 @@ namespace Rocketbox
             return true;
         }
     }
+
+    internal class ShellCommand : RbCommand
+    {
+        private string _command;
+
+        internal ShellCommand(string command)
+        {
+            _command = command;
+        }
+
+        // parameter can be null
+        public string GetResponse(string arguments)
+        {
+            return string.Format("Run: {0}", _command);
+        }
+
+        // parameter can be null
+        public bool Execute(string arguments)
+        {
+            // the entire command is passed into the constructor
+            // we need to differentiate between the process and the arguments because System.Diagnostics said so
+            string process = RbUtility.GetKeyword(_command);
+            string args = RbUtility.StripKeyword(_command);
+
+            ProcessStartInfo info = new ProcessStartInfo(@process);
+            info.Arguments = args;
+            try
+            {
+                Process.Start(info);
+            }
+            catch(Exception e)
+            {
+                // if launch fails, just do nothing
+                return false;
+            }
+
+            return true;
+        }
+    }
 }
