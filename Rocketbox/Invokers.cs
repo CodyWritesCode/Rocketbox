7374bf493bf03d9cf7c5f077114c465e886b8937
Cody <codywritescode@gmail.com>
2017-07-24 22:32:50 -0500

    add execution invoker + add search exec

--- Rocketbox/Invokers.cs
+++ Rocketbox/Invokers.cs
@@ -32,4 +32,37 @@ namespace Rocketbox
             return new NullCommand().GetResponse(command);
         }
     }
+
+    internal static class ActionInvoker
+    {
+        internal static bool Execute(string command)
+        {
+            if(command.Trim() == string.Empty)
+            {
+                // temporary
+                new NullCommand().Execute(command);
+                return false;
+            }
+
+            // TODO: move duplicate code
+            string keyword = RbUtility.GetKeyword(command);
+            string parameters = RbUtility.StripKeyword(command);
+
+            // first test for search engines
+            var matchingSearchEngines = from engine in RbData.SearchEngines
+                                        where engine.Keywords.Contains(keyword)
+                                        select engine;
+
+            if (matchingSearchEngines.Count() != 0)
+            {
+                SearchCommand searchCommand = new SearchCommand(matchingSearchEngines.First());
+                if(searchCommand.Execute(parameters))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
 }
