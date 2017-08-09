2244215c7e29adc5f9aa3c9fe7227d501ddc33b7
Cody <codywritescode@gmail.com>
2017-08-08 23:05:59 -0500

    add short "c" command for conversion

--- Rocketbox/Invoker.cs
+++ Rocketbox/Invoker.cs
@@ -56,6 +56,7 @@ namespace Rocketbox
                 case "CONVERT":
                 case "CON":
                 case "CV":
+                case "C":
                     _currentCmd = new UnitConversionCommand();
                     break;
             }
